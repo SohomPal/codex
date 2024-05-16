388b499e96ce8c77a92ddfb73731e65975f57d43
SohomPal <sohompal21@gmail.com>
2024-05-16 13:06:30 -0400

    first commit

---
+++ client/script.js
@@ -0,0 +1,104 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadIntveral;
+
+function loader(element){
+  element.textContent = '';
+  loadIntveral = setInterval(() => {
+    element.textContent += '.';
+
+    if(element.textContent === '....'){
+      element.textContent = '';
+    }
+
+  }, 300)
+}
+
+function typeText(element, text){
+  let index = 0; 
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }
+    else{
+      clearInterval(interval);
+    }
+  }, 19)
+}
+
+function generateUniqueID(){
+  const timeStamp = Date.now();
+  const rand = Math.random();
+  const hexDecimalString = rand.toString(16);
+
+  return `id-${timeStamp}-${hexDecimalString}`;
+}
+
+function chatStripe(isAI, value, uniqueID){
+  return(
+    `
+       <div class = "wrapper ${isAI && 'ai'}">
+        <div class = "chat">
+          <div class = "profile">
+            <img src = "${isAI ? bot : user}" alt = "${isAI ? "bot" : "user "}"
+            />
+          </div>
+          <div class = "message" id = ${uniqueID}> ${value} </div>
+        </div>
+       </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  //generate user's chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+
+  //generate bot's chat stripe
+  const uniqueID = generateUniqueID();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueID);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueID);
+  loader(messageDiv);
+
+  //fetch data from server (bot's response)
+
+  const response = await fetch('http://localhost:3000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt')
+      })
+  })
+
+  clearInterval(loadIntveral);
+  messageDiv.innerHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  }
+  else{
+    const error = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13)
+  handleSubmit(e);
+})
---
+++ server/server.js
@@ -0,0 +1,52 @@
+import  express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+//import OpenAI from "openai";
+import Groq from "groq-sdk";
+
+dotenv.config();
+
+// const openai = new OpenAI({
+//     apiKey: "sk-proj-vibIOUraRgr0PalliPTTT3BlbkFJ2EUeej4EfAxM8ucVnfE4",
+//   });
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: "Hello World"
+    })
+})
+
+app.post('/', async (req, res) => {
+    try{
+        
+        const prompt = req.body.prompt;
+
+        const response = await groq.chat.completions.create({
+            messages: [
+                {
+                    role: "user",
+                    content: `${prompt}`,
+                }
+            ],
+            model: "llama3-8b-8192"
+        });
+
+        res.status(200).send({
+            bot: response.choices[0].message.content
+        })
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).send({error});
+    }
+})
+
+app.listen(3000, () => {console.log('Server is listening on port http://localhost:3000 ')}) 
