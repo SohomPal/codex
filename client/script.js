c3934b1c231e25b5d0ca31264bf25624eeeefd3e
SohomPal <sohompal21@gmail.com>
2024-05-16 14:43:12 -0400

    update server url

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server (bot's response)
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://codex-5qc3.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import  express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-//import OpenAI from "openai";
 import Groq from "groq-sdk";
 
 dotenv.config();
